315188774b4f72bcae000bbbec3a447ea080109e
Chikezie Ugochukwu Emmanuel <codewithemma@gmail.com>
2024-06-02 02:42:17 +0100

    it was easy but i did it

--- src/app/news/dummy.jsx
+++ src/app/news/dummy.jsx
@@ -1,16 +1,16 @@
 export const posts = [
   {
-    img: "",
+    img: "/assets/image.png",
     description:
       "The Nigerian Parliament has passed a new electoral bill aimed at improving transparency and fairness in the electoral process.",
   },
   {
-    img: "",
+    img: "/assets/image.png",
     description:
       "The House of Representatives has commenced debates on the proposed national budget for the fiscal year 2024, focusing on key areas of development.",
   },
   {
-    img: "",
+    img: "/assets/image.png",
     description:
       "The Nigerian Senate has approved new security measures to combat rising insecurity and improve public safety across the nation.",
   },
--- src/app/news/page.jsx
+++ src/app/news/page.jsx
@@ -3,35 +3,56 @@ import styles from "./News.module.css";
 import Image from "next/image";
 import Menu from "@/components/menu/Menu";
 import NewsCard from "@/components/newsCard/NewsCard";
+import { posts } from "./dummy";
 const page = () => {
-  const data = [
-    { id: 1, content: "This div takes up 2fr of the space." },
-    {
-      id: 2,
-      content: "This div takes up 1fr and is a flex container.",
-      isFlex: true,
-      flexItems: ["Flex Item 1", "Flex Item 2"],
-    },
-    { id: 3, content: "This div also takes up 1fr of the space." },
-  ];
   return (
     <div>
       <div className={styles.bigBox}>
-        {data.map((item) => (
-          <div
-            key={item.id}
-            className={
-              item.id === 1
-                ? styles.box
-                : `${styles.box} ${styles.flexContainer}`
-            }
-          >
-            {item.content}
+        <div className={styles.largeBox}>
+          <div className={styles.news_img}>
+            <Image
+              src={posts[0].img}
+              alt="large"
+              fill
+              priority
+              style={{ objectFit: "cover" }}
+            />
+            <div className={styles.bg}>
+              <div style={{ width: "500px" }}>
+                <button className={styles.btn}>News</button>
+                <p>{posts[0].description}</p>
+                <Link href="/" className={styles.link}>
+                  Read more
+                </Link>
+              </div>
+            </div>
           </div>
-        ))}
+        </div>
+        <div className={`${styles.smallBox} ${styles.flexContainer}`}>
+          {posts?.slice(1).map((item) => (
+            <div className={`${styles.news_img1}`} key={item.id}>
+              <Image
+                src={item.img}
+                alt="large"
+                fill
+                priority
+                style={{ objectFit: "cover" }}
+              />
+              <div className={styles.bg1}>
+                <div style={{ width: "200px" }}>
+                  <button className={styles.btn}>News</button>
+                  <p>{posts[0].description}</p>
+                  <Link href="/" className={styles.link}>
+                    Read more
+                  </Link>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
       <p className={styles.header}>Parliamentary News</p>
-      <div className={styles.grid}>
+      {/* <div className={styles.grid}>
         <div className={styles.news_img}>
           <Image
             src="/assets/1723.jpg"
@@ -97,7 +118,7 @@ const page = () => {
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
       <div className={styles.box}>
         <p>Parliarmentary News From the House of Representatives</p>
       </div>
