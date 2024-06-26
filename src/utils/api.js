9f925e76eb60444eee942240ca120e5fb7c26024
Chikezie Ugochukwu Emmanuel <codewithemma@gmail.com>
2024-06-26 17:16:59 +0100

    cleanup

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,3 +1,3 @@
-const uri = "test-news-cyan.vercel.app";
+const uri = "https://test-news-cyan.vercel.app";
 const localUrl = "http://localhost:3000";
 export const url = process.env.NODE_ENV === "production" ? uri : localUrl;
